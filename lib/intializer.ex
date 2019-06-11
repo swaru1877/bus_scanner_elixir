0f24b81ffece957c33f08086ef25ddab10e37ab5
Anjusha <Anjusha@INskhandavallis-MacBook-Pro.local>
2019-06-11 12:02:07 +0530

    Adding intializer.ex to origin after adding right file path accesses for yaml file

--- lib/intializer.ex
+++ lib/intializer.ex
@@ -1,11 +1,10 @@
 defmodule Yamerl_init do
-	def func do
-		IO.puts "Calling init module"
-		:application.start(:yamerl)
-		[sites_info | _ ] = :yamerl_constr.file "/Users/Anjusha/Desktop/Sriram/bus_scanner_elixir/config/target_websites.yaml"
-		#sites_info
-		#|> Enum.map fn x -> IO.inspect(x) end
-		
-		
-	end
+  def func do
+    IO.puts("Calling init module")
+    yaml_path = Path.join([File.cwd!(), "config/target_websites.yaml"])
+    # The yamerl application must be started before any use of it.
+    :application.start(:yamerl)
+
+    [sites_info | _] = :yamerl_constr.file(yaml_path)
+  end
 end
